dcd535062eea4eef144fa4d53f74cf2ce236c289
kminoda <koji.m.minoda@gmail.com>
2019-12-16 13:01:50 +0100

    edit

--- src/control/control_hyper_parameters.m
+++
@@ -1,6 +0,0 @@
-% You can set any hyper parameters of the control function here; you may or
-% may not want to use the step_number as the input of the function. 
-function [?] = control_hyper_parameters(step_number)
-
-
-end
